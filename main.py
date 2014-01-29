2c99b726d0126f500e220214487823d2e463e65b
Lerikio <ssilfir@hotmail.fr>
2014-01-29 15:45:48 +0100

    Correction de l'importation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from logical_core import *
-from driver_first_try import Driver
+from src/logical_core import *
+from src/driver_first_try import Driver
 
 new_driver = driver_first_try.Driver()
 for action in new_driver.actuator.actions:
