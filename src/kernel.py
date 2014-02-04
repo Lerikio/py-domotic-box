cc066ea810c01775041caa8bab0f6e1d4cf88014
ralfiki <raphael.gautier@gmail.com>
2014-02-04 21:28:29 +0100

    dev

--- src/kernel.py
+++ src/kernel.py
@@ -1,33 +1,42 @@
 # -*- coding: utf-8 -*-
 
-# Classe gérant l'ensemble des plugins, des périphériques, des informations et des actions
-# Elle permet de charger de nouveaux plugins, de configurer de nouveaux périphériques,
-# de gérer la détection d'un nouveau périphérique que l'utilisateur devra configurer
 class Kernel:
+	""" Gathers every plugin installed. 
+	It centralizes every driver, modem, automaton and devices 
+	instantiated in the box to allow each other to communicate.
+	A plugin is instantiated once when it is loaded, the Kernel 
+	keeps a reference to it to avoid to load it again.
+	Drivers, modems and interfaces are also instantiated 
+	only once when the plugin it comes from is loaded.
+	Devices are instantiated by the drivers and added to the
+	Kernel's devices list to make them accessible to every 
+	other module. 
+	
+	
+	"""
 
 	def __init__(self):
 		self.plugins = []
 		self.drivers = []
+		self.modems = []
+		self.automatons = []
+		self.interfaces = []
 		self.devices = []
 		
-	# Charge un plugin, c'est-à-dire détecte le type de plugin (modem ou protocole dans un premier temps)
-	# puis charge ce qu'apporte ce plugin (nouveaux périphériques...)
-	def load_plugin(self, plugin_path):
-		plugin = __import__(plugin_path, fromlist=['o'])
-		if plugin not in self.plugins:
-			print plugin.introduce_plugin()
-			if(plugin.introduce_plugin()['type'] == 'protocol'):
-				print "Chargement d'un plugin de protocole"
-				self.drivers.append(plugin.Driver())
-				print "Le driver suivant vient d'être chargé : "
-				print self.drivers[len(self.drivers)-1]
-				newDevices = self.drivers[len(self.drivers)-1].get_devices()
-				print "Les nouvelles classes de périphériques suivantes sont disponibles : "
-				print newDevices
-			else:
-				print "Ce n'est pas un plugin de protocole"
-		else:
-			print "Ce plugin a déjà été chargé."
-
-	# def add_device():
-                
+	def load_plugins(self):
+		""" Loads all plugins available in the plugin package/subdirectory. 
+		If a plugin has already been loaded, it is ignored.
+		
+		"""
+		available_plugins = __import__('plugins', fromlist=['o'])
+		for plugin in available_plugins:
+			if plugin not in self.plugins:
+				self.plugins.append(plugin)
+				if plugin.PLUGIN_TYPE == 'protocol':
+					self.drivers.append(plugin.Driver())
+				else if plugin.PLUGIN_TYPE == 'modem':
+					self.modems.append(plugin.Modem())
+				else if plugin.PLUGIN_TYPE == 'automaton':
+					self.automatons.append(plugin.Automaton())
+				else if plugin.PLUGIN_TYPE == 'interface':
+					self.interfaces.append(plugin.Interface())
--- src/plugin_nexa.py
+++ src/plugin_nexa.py
@@ -7,26 +7,19 @@
 # périphériques qu'elle apporte.
 #---------------------------------------------------------------
 
+# TODO: réflechir à cette histoire de core_classes
 from core_classes import * 
 
-# Fonction accessible dès l'importation du module Python.
-# Elle permet d'identifier le plugin qui va être chargé 
-# et d'adapter ainsi son chargement en conséquence.
 PLUGIN_TYPE = 'protocol'
-PLUGIN_NAME = 'nexa'
-PLUGIN_DESCRIPTION = 'Plugin de type protocol permettant au coeur de gérer des périphériques Nexa.'
-PLUGIN_CLASS = Driver
+""" Module variable allowing the Kernel to determine the type
+of the plugin and to instantiate it accordingly. 
 
-def introduce_plugin():
-	return {
-		'type' : PLUGIN_TYPE, 
-		'name' : PLUGIN_NAME, 
-		'description' : PLUGIN_DESCRIPTION,
-		'class' : PLUGIN_CLASS} 
+"""
 
-# Classe instanciée une seule fois par le coeur et gérant 
-# toutes les requêtes concernant le protocole Nexa
 class Driver():
+	""" Main class of a protocol plugin.
+	
+	"""
 
 	#---------------------------------------------------------------
 	# Interface avec le kernel 
@@ -45,28 +38,28 @@ class Driver():
 										'zero' : [1,1,1,5],
 										'end' : [1,30]
 										},
-								'structure' : ['start'] + [('one','zero') for i in range(32)] + ['end']
+								'structure' : ['start'] + 32*[('one','zero')] + ['end']
 								}
-
-		# Ce sont les nouveaux périphériques qu'on propose au core
-		self.handled_device_types = (NexaSwitch, NexaVirtualRemote)
 
 		# Liste des périphériques connectés gérés par ce driver
 		self.devices = []
 		
 		# Paramètres à fournir pour configurer le driver
-		self.settings = {'modem' : {'name' : 'modem', 
-								'type' : '433_radio_modem',
-								'description' : 'Modem radio à utiliser avec ce driver'}}
+		self.settings = {'modem' : ('Modem', None)}
 		
 		# Liste des périphériques potentiellement gérables par ce driver
 		self.handled_devices = [NexaSwitch, NexaVirtualRemote]
-	
+		
+		# Le driver ne propose à l'extérieur d'instantier un périphérique
+		# seulement si cela a un sens. Ici, un NexaSwitch ne sera instantié
+		# que si on en détecte un, automatiquement.
+		self.instantiable_devices = [NexaVirtualRemote]
+			
 	def get_set_arguments(self):
 		""" Returns the arguments needed to set the driver.	"""
 		return self.settings
 	
-	def set(self, settings):
+	def set(self, args):
 		""" Sets the driver. Returns True if the setting was a success. 
 		Otherwise, it returns a tuple containing the setting which caused 
 		the issue.
@@ -79,43 +72,46 @@ class Driver():
 		
 		# On vérifie que les arguments donnés sont valides
 		retour = True
-		if settings['modem'].get_modem_infos() == 'radio_433': 
-			self.modem = settings['modem']
+		if args['modem'].get_modem_type() == 'radio_433': 
+			self.modem = args['modem']
 			# On spécifie au modem la structure de message qu'on attend au modem 
 			# pour qu'il nous transmette ce qui est nous est adressé
 			self.modem.attach(self, self.message_structure)
 		else:
-			retour = ('modem', "Le modem specifié n'est pas du bon type"))
-	
+			retour = ('modem', "Le modem specifié n'est pas du bon type")
 		return retour
 
-	def get_handled_devices(self):
-		""" Returns a list of dictionaries describing the device handled 
-		by the present driver. It doesn't return a direct reference to 
-		the device classes to ensure that the driver is the only one being 
-		able to instantiate a device (through the add_device method).
+	def get_instantiable_devices(self):
+		""" Returns a list of the devices instantiable by the user
+		of the driver. 
+		The list of user-instantiable devices is included in the 
+		list of the driver-handled devices but not necessarily 
+		equal.  
 		
 		"""
 		# On ne fournit pas directement la classe pour que le driver reste
 		# le seul à pouvoir instancier des périphériques
-		return map(Device.get_device_info, self.handled_devices)
+		return [i.device_infos for i in self.instantiable_devices]
 
-	def add_device(self, device_id, arguments):
+	def add_device(self, device_id, args):
 		""" Proxy method used to instantiate a new device. Returns True
 		if the instantiation has succeeded, otherwise the arguments which
 		caused an issue.
-		 
+ 		 
 		"""
-
 		retour = True
-		for Dev in self.handled_devices:
-			if Dev.get_device_info()['device_id'] == device_id:
-				# TODO Vérifier que les bons arguments sont fournis
-				# avec une fonction générique (ce problème se pose 
-				# souvent dans le programme)
-				new_device = Dev(arguments)
-				self.devices.append(new_device)
-				
+		if device_id == 0:
+			new_device = NexaVirtualRemote()
+			# TODO: Vérifier que les bons arguments sont fournis
+			# avec une fonction générique (ce problème se pose 
+			# souvent dans le programme)
+			new_device.set(self, args)
+			self.devices.append(new_device)
+		else:
+			retour = ('device_id', "device_id out of range")
+		
+		return retour
+	
 	def get_devices(self):
 		return self.devices
 
@@ -131,57 +127,158 @@ class Driver():
 	# La méthode doit être spécialisée pour traiter et décoder une trame Nexa
 	# Elle sera totalement rédigée quand on saura sous quelle forme le message est obtenu
 	def process_message(self, message):
-		# TODO Transformation de ce qui est reçu du modem en message de 32 bits
-		house_code = message[0:26]
-		group_code = message[26:27]
-		command = message[27:28]
-		unit_code = message[28:32]
+		# TODO: Transformation de ce qui est reçu du modem en message de 32 bits
+		# Transformation d'un tableau de bits en entiers
+		def from_bitfield_to_int(bitfield):
+			return sum([i*2**(len(bitfield)-1-idi) for idi, i in enumerate(bitfield)])
+		
+		house_code = from_bitfield_to_int(message[0:26])
+		group_code = message[26]
+		command = message[27]
+		unit_code = from_bitfield_to_int(message[28:32])
+		
+		device_found = False
 		for device in self.devices:
 			if device.house_code == house_code:
-				# TODO Il faudrait vérifier qu'on a bien à faire à un sensor
+				device_found = True
+				# TODO: Il faudrait vérifier qu'on a bien à faire à un sensor
 				device.update(command)
-
+		if not device_found:
+			new_device = NexaSwitch()
+			new_device.set(self, {
+								'name' : "New Nexa Switch",
+								'description' : "A new Nexa Switch has been detected and added to your devices.",
+								'location' : "Specify a location.",
+								'house_code' : house_code,
+								'group_code' : group_code,
+								'unit_code' : unit_code})
+			self.devices.append(new_device)
 	#---------------------------------------------------------------
 	# Interface avec les devices 
 	#---------------------------------------------------------------
 
-	def sendCommand(self, device, command):
+	def send_command(self, device, command):
+		
+		def from_int_to_bitfield(n):
+			return [1 if digit=='1' else 0 for digit in bin(n)[2:]]
+		
+		sequence = from_int_to_bitfield(device.house_code)
+		sequence.append(device.group_code)
+		sequence.append(command)
+		sequence.append(from_int_to_bitfield(device.unit_code))
+		
+		# TODO: transformer le bitfield séquence en une réelle 
+		# séquence radio de type (pulse, durée du pulse) à 
+		# envoyer au modem radio
+		# Pour cela, utiliser les conventions données dans 
+		# self.message_structure
+		radio_sequence = []
+
+		result = True
 		if(self.modem != None):
-			sequence = startSequence + device.house_code + device.group_code + command + device.unit_code + endSequence
-			self.modem.send(sequence)
-			result = True
+			self.modem.send(radio_sequence)
 		else:
-			result = False
+			result = ("Il faut définir un modem radio 433 pour ce driver !")
 		return result
+
+#---------------------------------------------------------------
 
 # Interrupteur Nexa classique on/off
 class NexaSwitch(Sensor):
 
-	def __init__(self, name, description, location, house_code, group_code, unit_code):
-		Sensor.__init__(name, location)
-		self.house_code = house_code
-		self.group_code = group_code
-		self.unit_code = unit_code
-		self.informations.append(Information("Bouton 1 de la télécommande", "", "state", ("on", "off")))
+	# Ce sont les attributs qui caractérisent ce type de périphérique
+	device_infos = {
+				'device_name' : "Nexa Switch",
+				'device_brand' : "Nexa",
+				'device_description' : "Interrupteur Nexa on/off",
+				'arguments' : {
+							'name' : ('string', 50),
+							'description' : ('string', 1000),
+							'location' : ('string', 100),
+							'house_code' : ('integer', (0,67108863)),
+							'group_code' : ('integer', (0,1)),
+							'unit_code' : ('integer', (0,15))}}
 
-# Permet de simuler une télécommande Nexa (id généré aléatoirement ou fourni par l'utilisateur)
+	def __init__(self, name, description, location, house_code, group_code, unit_code):
+		self.name = None
+		self.description = None
+		self.location = None
+		self.driver = None
+		
+		self.house_code = None
+		self.group_code = None
+		self.unit_code = None
+				
+		self.informations = []
+		self.informations.append(Information(
+											"Etat", 
+											"Décrit l'état de l'interrupteur", 
+											("state", ("on", "off"))))
+		
+	def set(self, driver, args):
+		# TODO: vérifier la cohérence des arguments fournis avec ce qui a été demandé
+		self.name = args['name']
+		self.description = args['description']
+		self.location = args['location']
+		self.driver = driver
+		
+		self.house_code = args['house_code']
+		self.group_code = args['group_code']
+		self.unit_code = args['unit_code']
+
+#---------------------------------------------------------------
+
+# Permet de simuler une télécommande Nexa 
 class NexaVirtualRemote(Actuator):
 
-	# Ce sont les attributs qui caractérisent le type de périphérique
-	device_name = "Nexa Virtual Remote"
-	device_brand = "Nexa"
-	device_description = "Simulation d'une télécommande deux états Nexa"
+	# Ce sont les attributs qui caractérisent ce type de périphérique
+	device_infos = {
+				'device_name' : "Nexa Virtual Remote",
+				'device_brand' : "Nexa",
+				'device_description' : "Simulation d'une télécommande deux états Nexa",
+				'arguments' : {
+							'name' : ('string', 50),
+							'description' : ('string', 1000),
+							'location' : ('string', 100),
+							'house_code' : ('integer', (0,67108863)),
+							'group_code' : ('integer', (0,1)),
+							'unit_code' : ('integer', (0,15))}}
 
-	def __init__(self, name, description, location, house_code, group_code, unit_code):
-		super(NexaVirtualRemote, self).__init__(name, description, location)
-		self.house_code = house_code
-		self.group_code = group_code
-		self.unit_code = unit_code
-		self.actions.append(Action("on", "appuyer sur le bouton on de la télécommande", self.switch_on, {}))
-		self.actions.append(Action("off", "appuyer sur le bouton off de la télécommande", self.switch_off, {}))
+	def __init__(self):
+		self.name = None
+		self.description = None
+		self.location = None
+		self.driver = None
+		
+		self.house_code = None
+		self.group_code = None
+		self.unit_code = None
+		
+		self.actions = []
+		self.actions.append(Action(
+								"on", 
+								"appuyer sur le bouton on de la télécommande", 
+								self.switch_on, 
+								{}))
+		self.actions.append(Action(
+								"off", 
+								"appuyer sur le bouton off de la télécommande", 
+								self.switch_off, 
+								{}))
+		
+	def set(self, driver, args):
+		# TODO: vérifier la cohérence des arguments fournis avec ce qui a été demandé
+		self.name = args['name']
+		self.description = args['description']
+		self.location = args['location']
+		self.driver = driver
+		
+		self.house_code = args['house_code']
+		self.group_code = args['group_code']
+		self.unit_code = args['unit_code']
 
 	def switch_on(self):
-		self.driver.sendCommand(self, 1)
+		self.driver.send_command(self, 1)
 
 	def switch_off(self):
-		self.driver.sendCommand(self, 0)
+		self.driver.send_command(self, 0)
